3ea68b493a8fa043f056462db3cdf686875a8ba4
Bulmang <hmk113737@gmail.com>
2022-10-17 23:33:39 +0900

    Initial Commit

---
+++ Taico/TaicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaicoApp.swift
+//  Taico
+//
+//  Created by 하명관 on 2022/10/17.
+//
+
+import SwiftUI
+
+@main
+struct TaicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
