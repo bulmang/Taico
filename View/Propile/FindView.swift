c8af2265c19e33c525019827d6c8abb4f3ae9503
Bulmang <hmk113737@gmail.com>
2022-10-27 23:31:14 +0900

    Find ID View Update

--- View/Propile/FindView.swift
+++ View/Propile/FindView.swift
@@ -8,8 +8,11 @@ import SwiftUI
 
 struct FindIdView: View {
     
-    @State private var number:String = ""
-    @State var ColorCircle : Bool = true
+    @State private var name: String = ""
+    @State private var birth: String = ""
+    @State private var number: String = ""
+    @State private var permission: String = ""
+    @State var ColorCircle : Bool = false
     @State var alert: Bool = false
     
     var body: some View {
@@ -49,68 +52,79 @@ struct FindIdView: View {
                 ZStack{
                     VStack{
                         AgreeButton(title: "휴대폰 본인 인증 서비스 이용약관 동의 (필수)", url: "https://bccard.com/html/individual/pop_up/pop_personal04.html", image:   "chevron.right")
+                            
                         
                         
                         AgreeButton(title: "휴대폰 통신사 이용약관 동의 \n(필수)", url: "https://bccard.com/html/individual/pop_up/pop_personal04.html", image:   "chevron.right")
-                        
-                        
-                        
-                        
-                        AgreeButton(title: "휴대폰 본인 인증 서비스 이용약관 동의 (필수)", url: "https://bccard.com/html/individual/pop_up/pop_personal04.html", image:   "chevron.right")
-                        
-                        
-                        AgreeButton(title: "휴대폰 본인 인증 서비스 이용약관 동의 (필수)", url: "https://bccard.com/html/individual/pop_up/pop_personal04.html", image:   "chevron.right")
-                        
-                    }
-                    .opacity(ColorCircle ? 0 : 1)
-                    
-                    VStack{
-                        CustomTextField(placeHolder: "이름", imageName: "person", bColor: "textColor1", tOpacity: 0.6, value: $number)
-                        
-                        
-                        CustomTextField(placeHolder: "생년월일 6자리", imageName: "birthday.cake", bColor: "textColor1", tOpacity: 0.6, value: $number)
-                        
-                        CustomTextField(placeHolder: "휴대폰 번호", imageName: "phone", bColor: "textColor1", tOpacity: 0.6, value: $number)
-                        HStack{
                             
-                            if number.isEmpty {
+                        
+                        
+                        
+                        
+                        AgreeButton(title: "휴대폰 본인 인증 서비스 이용약관 동의 (필수)", url: "https://bccard.com/html/individual/pop_up/pop_personal04.html", image:   "chevron.right")
+                            
+                        
+                        
+                        AgreeButton(title: "휴대폰 본인 인증 서비스 이용약관 동의 (필수)", url: "https://bccard.com/html/individual/pop_up/pop_personal04.html", image:   "chevron.right")
+                            
+                        VStack{
+                            CustomTextField(placeHolder: "이름", imageName: "person", bColor: "textColor1", tOpacity: 0.6, value: $name)
+                            
+                            
+                            CustomTextField(placeHolder: "생년월일 6자리", imageName: "birthday.cake", bColor: "textColor1", tOpacity: 0.6, value: $birth)
+                            
+                            CustomTextField(placeHolder: "휴대폰 번호", imageName: "phone", bColor: "textColor1", tOpacity: 0.6, value: $number)
+                            
+                            HStack{
                                 Text("인증번호")
                                     .foregroundColor(Color(.black).opacity((0.6)))
                                     .padding()
                                     .font(.system(size: 20))
-                            }
-                            
-                            TextField("", text: $number)
-                                .padding(.leading, 12)
-                                .font((.system(size: 20)))
-                                .frame(height: 45)
-                                .foregroundColor(Color(.black))
-                                
-
-                            
-                            Button("인증요청") {
-                                alert = true
-                            }
-                            .padding(.trailing)
-                            .alert("메시지\n 인증 메세지를 보냈습니다.", isPresented: $alert){
-                                Button("확인", role: .cancel){
+                                if permission.isEmpty {
                                     
+                                   
+                                }
+                                TextField("", text: $permission)
+                                    .padding(.leading, 12)
+                                    .font((.system(size: 20)))
+                                    .frame(height: 45)
+                                    .foregroundColor(Color(.black))
+                                
+                                    
+
+                                
+                                Button("인증요청") {
+                                    alert = true
+                                }
+                                .padding(.trailing)
+                                .alert("메시지\n 인증 메세지를 보냈습니다.", isPresented: $alert){
+                                    Button("확인", role: .cancel){
+                                        
+                                    }
                                 }
                             }
-                        }
-                        .overlay (
-                            Divider()
-                                .overlay(
-                                    Color(.black).opacity(0.6)
-                                )
-                                .frame(maxWidth: .infinity)
-                                .padding(.top,50)
-                                .padding(.horizontal)
-                        )
+                            .overlay (
+                                Divider()
+                                    .overlay(
+                                        Color(.black).opacity(0.6)
+                                    )
+                                    .frame(maxWidth: .infinity)
+                                    .padding(.top,50)
+                                    .padding(.horizontal)
+                            )
+                            Spacer()
 
-        
+                            if permission != "" {
+                                CustomButton(title: "인증확인", bgColor: "color2")
+                            }
+            
+                        }
+                        .opacity(ColorCircle ? 0 : 1)
                     }
-                    .opacity(ColorCircle ? 1 : 0)
+                    
+                    
+                    
+                    
                 }
                 
             }
